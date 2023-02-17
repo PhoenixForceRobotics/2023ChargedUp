734564caff4d23b7f8142795aefdf37c69d533de
uselessparkswing <117786123+uselessparkswing@users.noreply.github.com>
2023-02-17 14:03:28 -0800

    Finished SetArm command

--- src/main/java/frc/robot/commands/arm/SetArm.java
+++ src/main/java/frc/robot/commands/arm/SetArm.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.arm;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.ArmConstants;
@@ -19,6 +20,39 @@ public class SetArm extends CommandBase {
         double xDistanceToFulcrum = distanceFromBumperMeters + ArmConstants.DISTANCE_BUMPER_TO_FULCRUM;
         double yDistanceToFulcrum = distanceFromGroundMeters + ArmConstants.DISTANCE_GROUND_TO_FULCRUM; 
         this.targetLength = Math.sqrt(Math.pow(xDistanceToFulcrum, 2) + Math.pow(yDistanceToFulcrum, 2));
-        this.targetAngle = Math.atan(xDistanceToFulcrum / yDistanceToFulcrum); //  
+        this.targetAngle = Math.atan(xDistanceToFulcrum / yDistanceToFulcrum);
+
+        anglePID = new PIDController(ArmConstants.ROTATION_PID_P, ArmConstants.ROTATION_PID_I, ArmConstants.ROTATION_PID_D);
+        lengthPID = new PIDController(ArmConstants.EXTENSION_PID_P, ArmConstants.EXTENSION_PID_I, ArmConstants.EXTENSION_PID_D);
+    }
+
+    @Override
+    public void initialize()
+    {
+        anglePID.setSetpoint(targetAngle);
+        lengthPID.setSetpoint(targetLength);
+    }
+
+    @Override
+    public void execute()
+    {
+        double lengthOutput = MathUtil.clamp(lengthPID.calculate(arm.getExtensionLength()), -0.9, 0.9);
+        arm.setExtensionMetersPerSecond(lengthOutput);
+
+        double angleOutput = MathUtil.clamp(lengthPID.calculate(arm.getRotationAngle()), -0.9, 0.9);
+        arm.setRotationRadiansPerSecond(angleOutput);
+    }
+
+    @Override
+    public boolean isFinished()
+    {
+        return anglePID.atSetpoint() && lengthPID.atSetpoint();
+    }
+
+    @Override
+    public void end(boolean interrupted)
+    {
+        arm.setRotationRadiansPerSecond(0);
+        arm.setExtensionMetersPerSecond(0);
     }
 }
